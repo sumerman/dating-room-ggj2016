42437b731d2cf18dfdd4c51dddce4bfa6f4591ae
Valery Meleshkin <valery.meleshkin@gmail.com>
2016-01-31 00:17:11 +0100

    rejoin support

--- lib/dating_room/broker.ex
+++ lib/dating_room/broker.ex
@@ -5,11 +5,22 @@ defmodule DatingRoom.Broker do
   defmodule Redis do
     import Exredis.Api
 
+    def incr!(client, room) do
+      key = counter_name(room)
+      cmds = [
+        ["INCR",   key],
+        ["EXPIRE", key, hist_expire * 2],
+      ]
+      case Exredis.query_pipe(client, cmds) do
+        [id, "1"] -> String.to_integer(id)
+      end
+    end
+
     def send(client, room, message) do
       key = list_name(room)
       cmds = [
         ["LPUSH",   key, message],
-        ["LTRIM",   key, 0, hist_length],
+        ["LTRIM",   key, 0, hist_length - 1],
         ["EXPIRE",  key, hist_expire],
         ["PUBLISH", psub_name(room), message]
       ]
@@ -33,8 +44,10 @@ defmodule DatingRoom.Broker do
       end
     end
 
-    defp list_name(room), do: "room{#{room}}list"
     def  psub_name(room), do: "room{#{room}}"
+    defp list_name(room), do: "room{#{room}}list"
+    defp counter_name(room), do: "room{#{room}}counter"
+
     defp hist_length, do: 100
     defp hist_expire, do: 1800
     def  redis_uri, do: Application.get_env(:dating_room, :redis_uri, "")
@@ -55,10 +68,10 @@ defmodule DatingRoom.Broker do
     def to_redis(%Message{id: id, payload: payload}) when is_integer(id),
      do: "#{id}@#{payload}"
 
-    def from_redis(data) do
+    def from_redis!(data, room) do
       case String.split(data, "@", parts: 2) do
         [id, payload] ->
-          %Message{id: String.to_integer(id), payload: payload}
+          %Message{id: String.to_integer(id), payload: payload, room: room}
         _ -> raise ArgumentError
       end
     end
@@ -67,18 +80,19 @@ defmodule DatingRoom.Broker do
   def start_link(opts \\ []),
    do: GenServer.start_link(__MODULE__, [], [name: __MODULE__] ++ opts)
 
-  def send_to(room, payload) do
-    msg_id = :erlang.unique_integer([:positive, :monotonic])
-    send(%Message{id: msg_id, room: room, payload: payload})
+  def send_to!(room, f) do
+    redis = Process.whereis(:broker_redis_client)
+    msg_id = Redis.incr!(redis, room)
+    payload = f.(msg_id)
+    msg = Message.to_redis(%Message{id: msg_id, room: room, payload: payload})
+    Redis.send(redis, room, msg)
+    :ok
   end
 
-  def send(%Message{room: room} = message),
-   do: Process.whereis(:broker_redis_client) |> Redis.send(room, Message.to_redis(message))
-
-  def subscribe(room, proc \\ nil)
-  def subscribe(room, nil), do: subscribe(room, self)
-  def subscribe(room, pid) when is_pid(pid) do
-    case GenServer.call(__MODULE__, {:subscribe, room, pid}) do
+  def subscribe(room, last_seen \\ -1, proc \\ nil)
+  def subscribe(room, last_seen, nil), do: subscribe(room, last_seen, self)
+  def subscribe(room, last_seen, pid) when is_pid(pid) do
+    case GenServer.call(__MODULE__, {:subscribe, room, last_seen, pid}) do
       {:error, _} = err -> err;
       :ok ->
         bref = Process.monitor(__MODULE__)
@@ -105,12 +119,31 @@ defmodule DatingRoom.Broker do
                  }}
   end
 
-  def handle_call({:subscribe, room, pid}, _from, state) do
-    # TODO checkout history
-    unless :ets.member(state.subscribers, pid), do: Process.monitor(pid)
-    :ets.insert(state.subscribers,   {pid, room})
-    :ets.insert(state.subscribtions, {room, pid})
-    {:reply, :ok, state}
+  defp rejoin(_room, last_seen, _pid, _state) when last_seen <= 0, do: :ok
+  defp rejoin(room, last_seen, pid, state) do
+    missed = Redis.raw_history(state.redis_client, room)
+    |> Enum.map(&(Message.from_redis!(&1, room)))
+    |> Enum.take_while(&(&1.id >= last_seen))
+    |> Enum.reverse
+
+    case missed do
+      [%Message{id: id} | rest] when id == last_seen ->
+        Enum.each(rest, &(send(pid, &1)))
+        :ok
+      _ -> {:error, :missed_to_much}
+    end
+  end
+
+  def handle_call({:subscribe, room, last_seen, pid}, _from, state) do
+    case rejoin(room, last_seen, pid, state) do
+      {:error, _} = err ->
+        {:reply, err, state}
+      :ok ->
+        unless :ets.member(state.subscribers, pid), do: Process.monitor(pid)
+        :ets.insert(state.subscribers,   {pid, room})
+        :ets.insert(state.subscribtions, {room, pid})
+        {:reply, :ok, state}
+      end
   end
 
   def handle_call({:unsubscribe, room, pid}, _from, state) do
@@ -121,7 +154,7 @@ defmodule DatingRoom.Broker do
   def handle_info({:subscribed, "room*", _pid}, state), do: {:noreply, state}
   def handle_info({:pmessage, "room*", psub_room_name, data, _pid}, state) do
     room = Redis.name_from_psub(psub_room_name)
-    msg = data |> Message.from_redis |> Map.put(:room, room)
+    msg = data |> Message.from_redis!(room)
     for {_room, pid} <- :ets.lookup(state.subscribtions, room), do: send(pid, msg)
     {:noreply, state}
   end
--- lib/dating_room/websocket_handler.ex
+++ lib/dating_room/websocket_handler.ex
@@ -1,8 +1,11 @@
 defmodule DatingRoom.WebsocketHandler do
   @behaviour :cowboy_websocket_handler
 
+  alias DatingRoom.Broker
+  alias DatingRoom.Broker.Message
+
   defmodule State do
-    defstruct frametype: :binary
+    defstruct frametype: :binary, user_id: "", subscription: nil
   end
 
   def init({_tcp, _http}, _req, _opts),
@@ -25,38 +28,38 @@ defmodule DatingRoom.WebsocketHandler do
 
   def websocket_handle(_data, req, state), do: {:ok, req, state}
 
-  def websocket_info({:message, {:joined, room}}, req, state) do
-    users = :pg2.get_members({:room, room}) |> Enum.map(&inspect/1)
-    reply = %{type: "joined", users: users} |> Poison.encode!
-    {:reply, {state.frametype, reply}, req, state}
-  end
-  def websocket_info({:message, bin}, req, state),
+  def websocket_info(%Message{payload: bin}, req, state),
    do: {:reply, {state.frametype, bin}, req, state}
+  # broker down
+  def websocket_info({:DOWN, _ref, :process, pid, _reason}, req, state),
+   do: {:shutdown, req, state}
   def websocket_info(_info, req, state), do: {:ok, req, state}
 
-  defp handle_message(%{"type" => "join", "room" => room}, state) do
-    case :pg2.join({:room, room}, self) do
-      :ok -> :ok
-      {:error, _} -> :pg2.create({:room, room})
+  defp handle_message(%{"type" => "join", "room" => room, "user_id" => user_id} = msg, state) do
+    if state.subscription, do: Broker.unsubscribe(state.subscription)
+    last_id = Map.get(msg, "last_id", 0)
+    case Broker.subscribe(room, last_id) do
+      {:error, _} = err ->
+        {:reply, %{type: "error", reason: "#{inspect err}"}, state}
+      {:ok, subscr} ->
+        send_to! room, %{type: "joined", user_id: user_id}
+        {:ok, %{state | user_id: user_id, subscription: subscr}}
     end
-    :ok = :pg2.join({:room, room}, self)
-    send_all({:joined, room}, room)
-    {:ok, state}
   end
+
   defp handle_message(%{"type" => "send", "room" => room, "payload" => payload}, state) do
-    msg_id = :erlang.unique_integer([:positive, :monotonic])
-    %{type: "message", room: room, payload: payload,
-      id: msg_id, user_id: inspect(self)}
-      |> Poison.encode!
-      |> send_all(room)
+    send_to! room, %{type: "message", payload: payload, user_id: state.user_id}
     {:ok, state}
   end
-  # TODO handle broker down
   defp handle_message(msg, state),
    do: {:reply, %{type: "error", reason: "uknown msg", payload: msg}, state}
 
-  defp send_all(msg_bin, room) do
-    for pid <- :pg2.get_members({:room, room}), do: send(pid, {:message, msg_bin})
+  defp send_to!(room, message) do
+    Broker.send_to! room, fn msg_id ->
+      message
+      |> Map.put(:room, room)
+      |> Map.put(:id, msg_id)
+      |> Poison.encode!
+    end
   end
-
 end
